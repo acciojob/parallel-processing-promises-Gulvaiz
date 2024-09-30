0b39fb6693da363d0a510920fe2718622ac2904a
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-30 17:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImg(imagesURL){
+	 return new Promies((resolve, reject) => {
+		    if(imagesURL){
+				resolve output
+			}
+		 else {
+			  reject (e)
+		 }
+	 })
+}
+
+btn.addEventListener("click", () => {
+	 loadImg(images)
+	.then( (output) => {
+		output.innerHTML = `<img src="images"></img>`
+	})
+	.catch( (error) => output.innerText = `Failed to load image's URL: ${image.url}`)
+		
+	}
+})
